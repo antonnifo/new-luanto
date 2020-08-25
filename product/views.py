d812c2c338f6c2fe6c339f8182db640c07f71ab1
antonnifo <antonnifo@live.com>
2020-08-25 18:17:45 +0300

    add view function for home

--- product/views.py
+++ product/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category, ProductImage
 
-# Create your views here.
+
+PRODUCTS = Product.is_available.all()
+def index(request):
+    
+    
+    return render(request,
+        'product/index.html', {
+            'trending': PRODUCTS[:8]
+        })
+
+
+def product_list(request):
+
+   
+    return render(request,
+      'product/product_list.html',
+      {"products" : PRODUCTS})
+
+def product_detail(request, id, slug):
+
+    product = get_object_or_404(Product,
+                            id=id,
+                            slug=slug,
+                            available=True)
+
+    return render(request,
+              'product/product_detail.html',
+              {
+                  'product': product,              
+               })
