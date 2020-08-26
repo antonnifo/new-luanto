75b0c91c56a18fbdaf31e217a847ebe0903af1cb
antonnifo <antonnifo@live.com>
2020-08-26 10:59:54 +0300

    polish product detail view

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category, ProductImage
+from django.db.models import Count
 
 
 PRODUCTS = Product.is_available.all()
@@ -26,8 +27,19 @@ def product_detail(request, id, slug):
                             slug=slug,
                             available=True)
 
+    # related products
+     
+    product_tags_ids = product.tags.values_list('id', flat=True)
+    
+    related_products = PRODUCTS.filter(tags__in=product_tags_ids)\
+                        .exclude(id=product.id)
+
+    related_products = related_products.annotate(same_tags=Count('tags')).order_by('-same_tags','-updated')[:4]
     return render(request,
               'product/product_detail.html',
               {
-                  'product': product,              
+                  'product': product, 
+                  'product_images': ProductImage.objects.filter(product=product),
+                  'tags': product.tags.all(),
+                  'related_products' :  related_products,            
                })
