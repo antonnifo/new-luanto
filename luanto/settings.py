9b1158659055c9f694875fb77e1d25a8f6248134
antonnifo <antonnifo@live.com>
2020-08-27 10:16:26 +0300

    manage the cart

--- luanto/settings.py
+++ luanto/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'product',
     'taggit',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,3 +118,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 TAGGIT_CASE_INSENSITIVE = True
+
+CART_SESSION_ID = 'cart'
